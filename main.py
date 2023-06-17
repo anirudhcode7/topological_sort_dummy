49681e697246a7baa6c944d6a773059f00dd1ddd
anirudhcode7 <37476292+anirudhcode7@users.noreply.github.com>
2023-06-18 00:26:12 +0530

    DOPS-1862: Artifacts and Dependencies for each project.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import json
-from xml.etree import ElementTree as ET
 import subprocess
 
 
@@ -8,6 +7,20 @@ def extract_xpath_value(pom_file, xpath_expression):
     command = ['./sh/extract_xpath_value.sh', pom_file, xpath_expression]
     output = subprocess.check_output(command, universal_newlines=True)
     return output.strip()
+
+
+def get_package_name(package_json_file):
+    with open(package_json_file) as file:
+        data = json.load(file)
+        return data.get("name", "")
+
+
+def get_dependencies(package_json_file):
+    with open(package_json_file) as file:
+        data = json.load(file)
+        dependencies = data.get("dependencies", {})
+        print(dependencies)
+        return dependencies
 
 
 def parse_pom_artifacts(pom_file):
@@ -19,17 +32,32 @@ def parse_pom_artifacts(pom_file):
     return artifacts
 
 
-def parse_all_pom_artifacts(folder_path):
+def parse_pom_dependencies(pom_file):
+    dependencies = []
+    command = ['./sh/extract_internal_dependencies.sh', pom_file]
+    output = subprocess.check_output(command, universal_newlines=True)
+    if output is not None and len(output) != 0:
+        dependencies = output.strip().split('\n')
+    dependencies = [dep for dep in dependencies if dep]
+    return dependencies
+
+
+def parse_all_pom_artifacts_and_dependencies(folder_path):
     artifacts_per_project = {}
+    dependencies_per_project = {}
     for project_name in os.listdir(folder_path):
         project_path = os.path.join(folder_path, project_name)
         if project_name not in artifacts_per_project.keys():
             artifacts_per_project[project_name] = []
+        if project_name not in dependencies_per_project.keys():
+            dependencies_per_project[project_name] = []
         if os.path.isdir(project_path):
             pom_file = os.path.join(project_path, "pom.xml")
             if os.path.isfile(pom_file):
                 artifacts = parse_pom_artifacts(pom_file)
                 artifacts_per_project[project_name].extend(artifacts)
+                dependencies = parse_pom_dependencies(pom_file)
+                dependencies_per_project[project_name].extend(dependencies)
 
             for sub_directory_name in os.listdir(project_path):
                 sub_directory_path = os.path.join(project_path, sub_directory_name)
@@ -38,14 +66,16 @@ def parse_all_pom_artifacts(folder_path):
                     if os.path.isfile(pom_file):
                         artifacts = parse_pom_artifacts(pom_file)
                         artifacts_per_project[project_name].extend(artifacts)
-    return artifacts_per_project
+                        dependencies = parse_pom_dependencies(pom_file)
+                        dependencies_per_project[project_name].extend(dependencies)
 
-def parse_pom_dependencies(pom_file):
-    command = ['./sh/extract_internal_dependencies.sh', pom_file]
-    output = subprocess.check_output(command, universal_newlines=True)
-    dependencies = output.strip().split('\n')
-    return dependencies
+    for key, value in artifacts_per_project.items():
+        artifacts_per_project[key] = list(set(value))
 
+    for key, value in dependencies_per_project.items():
+        dependencies_per_project[key] = list(set(value))
+
+    return artifacts_per_project, dependencies_per_project
 
 
 def find_projects(folder_path):
@@ -104,18 +134,21 @@ def read_projects_from_json(json_file_path):
 clone_dir = os.path.abspath("/home/anirudh.ponna/git/test/cloned_projects_full")
 
 # Find projects in the folder
-# projects = find_projects(clone_dir)
+projects = find_projects(clone_dir)
 
-# artifacts = parse_all_pom_artifacts(clone_dir)
+artifacts, dependencies = parse_all_pom_artifacts_and_dependencies(clone_dir)
 
-dependencies = parse_pom_dependencies('/home/anirudh.ponna/git/test/cloned_projects_full/avx_scheduler/crontab-mgmt/pom.xml')
-print(dependencies)
+# dependencies = parse_pom_dependencies(
+#     '/home/anirudh.ponna/git/test/cloned_projects_full/avx_scheduler/crontab-mgmt/pom.xml')
+# print(dependencies)
 
 # Dump projects to a JSON file
 json_file_path = "./projects.json"
 json_artifact_path = "./artifacts.json"
-# dump_projects_to_json(projects, json_file_path)
-# dump_projects_to_json(artifacts, json_artifact_path)
+json_dependencies_path = "./dependencies.json"
+dump_projects_to_json(projects, json_file_path)
+dump_projects_to_json(artifacts, json_artifact_path)
+dump_projects_to_json(dependencies, json_dependencies_path)
 
 # Read projects from the JSON file
 projects = read_projects_from_json(json_file_path)
--- sh/extract_internal_dependencies.sh
+++ sh/extract_internal_dependencies.sh
@@ -2,6 +2,8 @@
 
 pom_file=$1
 
-xmlstarlet sel -N ns="http://maven.apache.org/POM/4.0.0" -t \
-  -m "//ns:dependencies/ns:dependency[contains(ns:groupId, 'appviewx')]" \
-  -v "ns:artifactId" -n "$pom_file"
+value=$(xmlstarlet sel -N ns="http://maven.apache.org/POM/4.0.0" -t \
+  -m "//ns:dependencies/ns:dependency[not(ancestor::ns:dependencyManagement) and contains(ns:groupId, 'appviewx')]" \
+  -v "ns:artifactId" -n "$pom_file")
+
+echo "$value"
