99b5ec122e6f039061c886567e7a7aaf14c23907
anirudhcode7 <37476292+anirudhcode7@users.noreply.github.com>
2023-06-18 16:21:38 +0530

    DOPS-1862: Levels for each project

--- main.py
+++ main.py
@@ -1,6 +1,112 @@
+import logging
 import os
 import json
 import subprocess
+import sys
+
+from sqlalchemy import create_engine, text, insert
+
+
+# from db.src.postgres.common.sqlAlchemy import SQLAlchemyProcessor as PostgresProcessor
+
+
+def connect_to_db():
+    dbUserName = os.environ.get('dbUserName')
+    dbPassword = os.environ.get('dbPassword')
+    dbPort = os.environ.get('dbPort')
+    dbHost = os.environ.get('dbHost')
+    dbSchema = 'prod'
+    dbURI = f"postgresql://{dbUserName}:{dbPassword}@{dbHost}:{dbPort}/{dbSchema}"
+    logging.debug(f"dbURI: {dbURI} ")
+    sqlp = create_engine(dbURI, echo=True)
+    return sqlp
+
+
+def getResultsFromSQL(sqlp, sqlQuery):
+    str_sql = text(sqlQuery)
+    results = sqlp.connect().execute(str_sql).fetchall()
+    return results
+
+
+def get_build_hierarchy(sqlp):
+    sql_query = "select git_repo_name from release_build_hierarchy where release_collection_id=1"
+    projects = getResultsFromSQL(sqlp, sql_query)
+    projects = [result[0] for result in projects]
+    return projects
+
+
+def change_to_list_and_dump_to_json(dictionary, filename):
+    dictionary_list_val = {}
+    for key, val in dictionary.items():
+        if type(val) == set:
+            dictionary_list_val[key] = list(val)
+        else:
+            dictionary_list_val[key] = val
+
+    dump_projects_to_json(dictionary_list_val, filename)
+
+
+def remove_self_dependencies(adjacency_list):
+    adjacency_list_without_self_loops = {}
+    for key, val in adjacency_list.items():
+        adjacency_list_without_self_loops[key] = set()
+        for dependency in val:
+            if dependency == key:
+                continue
+            adjacency_list_without_self_loops[key].add(dependency)
+    return adjacency_list_without_self_loops
+
+
+def build_order(artifacts_per_project, dependencies_per_project):
+    # Create a reverse mapping of artifacts to projects
+    artifact_to_project = {}
+    for project, artifacts in artifacts_per_project.items():
+        for artifact in artifacts:
+            artifact_to_project[artifact] = project
+
+    change_to_list_and_dump_to_json(artifact_to_project, './artifacts_to_project.json')
+    adjacency_list = {}
+    for project, dependencies in dependencies_per_project.items():
+        adjacency_list[project] = set()
+        for dependency in dependencies:
+            if dependency != project and dependency in artifact_to_project:
+                adjacency_list[project].add(artifact_to_project[dependency])
+
+    adjacency_list = remove_self_dependencies(adjacency_list)
+    change_to_list_and_dump_to_json(adjacency_list, './project_to_project.json')
+
+    # Initialize a dictionary to store the levels of each project
+    project_to_levels = {}
+    levels_to_project = {}
+    level = 0
+
+    # Perform topological sorting
+    while adjacency_list:
+        # Find projects with no remaining dependencies
+        projects_with_no_dependencies = [
+            project for project, dependencies in adjacency_list.items() if len(dependencies) == 0
+        ]
+
+        if not projects_with_no_dependencies:
+            # There are remaining dependencies, but they form a cycle
+            raise Exception("Dependency cycle detected")
+
+        # Remove the projects with no dependencies from the adjacency list
+        for project in projects_with_no_dependencies:
+            del adjacency_list[project]
+            project_to_levels[project] = level
+            if level not in levels_to_project:
+                levels_to_project[level] = []
+            levels_to_project[level].append(project)
+
+            # Remove the project from the dependencies of other projects
+            for dependencies in adjacency_list.values():
+                dependencies.discard(project)
+
+        level += 1
+
+    change_to_list_and_dump_to_json(levels_to_project, "./levels_to_project.json")
+    return levels_to_project
 
 
 def extract_xpath_value(pom_file, xpath_expression):
@@ -44,47 +150,48 @@ def parse_pom_dependencies(pom_file):
     return dependencies
 
 
-def parse_all_artifacts_and_dependencies(folder_path):
+def parse_all_artifacts_and_dependencies(folder_path, build_hierarchy_projects):
     artifacts_per_project = {}
     dependencies_per_project = {}
     for project_name in os.listdir(folder_path):
-        project_path = os.path.join(folder_path, project_name)
-        if project_name not in artifacts_per_project.keys():
-            artifacts_per_project[project_name] = []
-        if project_name not in dependencies_per_project.keys():
-            dependencies_per_project[project_name] = []
-        if os.path.isdir(project_path):
-            pom_file = os.path.join(project_path, "pom.xml")
-            package_file = os.path.join(project_path, "package.json")
+        if project_name in build_hierarchy_projects:
+            project_path = os.path.join(folder_path, project_name)
+            if project_name not in artifacts_per_project.keys():
+                artifacts_per_project[project_name] = []
+            if project_name not in dependencies_per_project.keys():
+                dependencies_per_project[project_name] = []
+            if os.path.isdir(project_path):
+                pom_file = os.path.join(project_path, "pom.xml")
+                package_file = os.path.join(project_path, "package.json")
 
-            if os.path.isfile(pom_file):
-                artifacts = parse_pom_artifacts(pom_file)
-                artifacts_per_project[project_name].extend(artifacts)
-                dependencies = parse_pom_dependencies(pom_file)
-                dependencies_per_project[project_name].extend(dependencies)
+                if os.path.isfile(pom_file):
+                    artifacts = parse_pom_artifacts(pom_file)
+                    artifacts_per_project[project_name].extend(artifacts)
+                    dependencies = parse_pom_dependencies(pom_file)
+                    dependencies_per_project[project_name].extend(dependencies)
 
-            if os.path.isfile(package_file):
-                artifacts = parse_npm_artifacts(package_file)
-                artifacts_per_project[project_name].extend(artifacts)
-                dependencies = parse_npm_dependencies(package_file)
-                dependencies_per_project[project_name].extend(dependencies)
+                if os.path.isfile(package_file):
+                    artifacts = parse_npm_artifacts(package_file)
+                    artifacts_per_project[project_name].extend(artifacts)
+                    dependencies = parse_npm_dependencies(package_file)
+                    dependencies_per_project[project_name].extend(dependencies)
 
-            for sub_directory_name in os.listdir(project_path):
-                sub_directory_path = os.path.join(project_path, sub_directory_name)
-                if os.path.isdir(sub_directory_path):
-                    pom_file = os.path.join(sub_directory_path, "pom.xml")
+                for sub_directory_name in os.listdir(project_path):
+                    sub_directory_path = os.path.join(project_path, sub_directory_name)
+                    if os.path.isdir(sub_directory_path):
+                        pom_file = os.path.join(sub_directory_path, "pom.xml")
 
-                    if os.path.isfile(pom_file):
-                        artifacts = parse_pom_artifacts(pom_file)
-                        artifacts_per_project[project_name].extend(artifacts)
-                        dependencies = parse_pom_dependencies(pom_file)
-                        dependencies_per_project[project_name].extend(dependencies)
+                        if os.path.isfile(pom_file):
+                            artifacts = parse_pom_artifacts(pom_file)
+                            artifacts_per_project[project_name].extend(artifacts)
+                            dependencies = parse_pom_dependencies(pom_file)
+                            dependencies_per_project[project_name].extend(dependencies)
 
-                    if os.path.isfile(package_file):
-                        artifacts = parse_npm_artifacts(package_file)
-                        artifacts_per_project[project_name].extend(artifacts)
-                        dependencies = parse_npm_dependencies(package_file)
-                        dependencies_per_project[project_name].extend(dependencies)
+                        if os.path.isfile(package_file):
+                            artifacts = parse_npm_artifacts(package_file)
+                            artifacts_per_project[project_name].extend(artifacts)
+                            dependencies = parse_npm_dependencies(package_file)
+                            dependencies_per_project[project_name].extend(dependencies)
 
     for key, value in artifacts_per_project.items():
         artifacts_per_project[key] = list(set(value))
@@ -103,45 +210,46 @@ def parse_all_artifacts_and_dependencies(folder_path):
     return artifacts_per_project, dependencies_per_project
 
 
-def find_projects(folder_path):
+def find_projects(folder_path, build_hierarchy_projects):
     projects = []
 
     for project_name in os.listdir(folder_path):
-        project_path = os.path.join(folder_path, project_name)
-        if os.path.isdir(project_path):
-            pom_file = os.path.join(project_path, "pom.xml")
-            package_file = os.path.join(project_path, "package.json")
+        if project_name in build_hierarchy_projects:
+            project_path = os.path.join(folder_path, project_name)
+            if os.path.isdir(project_path):
+                pom_file = os.path.join(project_path, "pom.xml")
+                package_file = os.path.join(project_path, "package.json")
 
-            if os.path.isfile(pom_file):
-                projects.append({
-                    "type": "java",
-                    "name": project_name,
-                    "pom_file": pom_file
-                })
-            elif os.path.isfile(package_file):
-                projects.append({
-                    "type": "node",
-                    "name": project_name,
-                    "package_file": package_file
-                })
+                if os.path.isfile(pom_file):
+                    projects.append({
+                        "type": "java",
+                        "name": project_name,
+                        "pom_file": pom_file
+                    })
+                elif os.path.isfile(package_file):
+                    projects.append({
+                        "type": "node",
+                        "name": project_name,
+                        "package_file": package_file
+                    })
 
-            for sub_dir_name in os.listdir(project_path):
-                sub_dir_path = os.path.join(project_path, sub_dir_name)
-                if os.path.isdir(sub_dir_path):
-                    pom_file = os.path.join(sub_dir_path, "pom.xml")
-                    package_file = os.path.join(sub_dir_path, "package.json")
-                    if os.path.isfile(pom_file):
-                        projects.append({
-                            "type": "java",
-                            "name": project_name,
-                            "pom_file": pom_file
-                        })
-                    elif os.path.isfile(package_file):
-                        projects.append({
-                            "type": "node",
-                            "name": project_name,
-                            "package_file": package_file
-                        })
+                for sub_dir_name in os.listdir(project_path):
+                    sub_dir_path = os.path.join(project_path, sub_dir_name)
+                    if os.path.isdir(sub_dir_path):
+                        pom_file = os.path.join(sub_dir_path, "pom.xml")
+                        package_file = os.path.join(sub_dir_path, "package.json")
+                        if os.path.isfile(pom_file):
+                            projects.append({
+                                "type": "java",
+                                "name": project_name,
+                                "pom_file": pom_file
+                            })
+                        elif os.path.isfile(package_file):
+                            projects.append({
+                                "type": "node",
+                                "name": project_name,
+                                "package_file": package_file
+                            })
     return projects
 
 
@@ -156,24 +264,34 @@ def read_projects_from_json(json_file_path):
     return projects
 
 
-clone_dir = os.path.abspath("/home/anirudh.ponna/git/test/cloned_projects_full")
+if __name__ == "__main__":
+    sqlp = connect_to_db()
+    build_hierarchy_projects = get_build_hierarchy(sqlp)
+    clone_dir = os.path.abspath("/home/anirudh.ponna/git/test/cloned_projects_full")
 
-# Find projects in the folder
-projects = find_projects(clone_dir)
+    # Find projects in the folder
+    projects = find_projects(clone_dir, build_hierarchy_projects)
 
-artifacts, dependencies = parse_all_artifacts_and_dependencies(clone_dir)
+    artifacts, dependencies = parse_all_artifacts_and_dependencies(clone_dir, build_hierarchy_projects)
+    project_to_project = build_order(artifacts, dependencies)
+    # dependencies = parse_pom_dependencies(
+    #     '/home/anirudh.ponna/git/test/cloned_projects_full/avx_scheduler/crontab-mgmt/pom.xml')
+    # print(dependencies)
 
-# dependencies = parse_pom_dependencies(
-#     '/home/anirudh.ponna/git/test/cloned_projects_full/avx_scheduler/crontab-mgmt/pom.xml')
-# print(dependencies)
+    # Dump projects to a JSON file
+    json_file_path = "./projects.json"
+    json_artifact_path = "./artifacts.json"
+    json_dependencies_path = "./dependencies.json"
+    json_artifacts_to_project = "./artifacts_to_project.json"
+    json_project_to_project = "./project_to_project.json"
+    dump_projects_to_json(projects, json_file_path)
+    dump_projects_to_json(artifacts, json_artifact_path)
+    dump_projects_to_json(dependencies, json_dependencies_path)
+    # dump_projects_to_json(artifacts_to_project, json_artifacts_to_project)
+    dump_projects_to_json(project_to_project, json_project_to_project)
 
-# Dump projects to a JSON file
-json_file_path = "./projects.json"
-json_artifact_path = "./artifacts.json"
-json_dependencies_path = "./dependencies.json"
-dump_projects_to_json(projects, json_file_path)
-dump_projects_to_json(artifacts, json_artifact_path)
-dump_projects_to_json(dependencies, json_dependencies_path)
+    print("Artifacts length:", len(artifacts))
+    print("Dependencies length:", len(dependencies))
 
-# Read projects from the JSON file
-projects = read_projects_from_json(json_file_path)
+    # Read projects from the JSON file
+    projects = read_projects_from_json(json_file_path)
